f44e62f54d709990459d69f461e9039f20d3a472
Sam Richards <sam@meridianspacecommand.com>
2024-12-06 11:29:19 +0000

    moved ATS process to seperate thread to allow manual commanding during ATS running, and added STOP_ATS command to kill that ATS thread.

--- SIM/apps/spacecraft/obc.py
+++ SIM/apps/spacecraft/obc.py
@@ -4,6 +4,7 @@ from config import SPACECRAFT_CONFIG, SIM_CONFIG
 import numpy as np
 import time
 import csv
+import threading
 
 class OBCModule:
     def __init__(self):
@@ -21,6 +22,11 @@ class OBCModule:
         # Initialize time variables
         self.current_sim_time = SIM_CONFIG['mission_start_time']
         self.epoch = SIM_CONFIG['epoch']
+        
+        # ATS handling
+        self.ats_thread = None
+        self.ats_running = False
+        self.ats_commands = []
 
     def update(self, current_time):
         """Update OBC state with current simulation time"""
@@ -84,16 +90,122 @@ class OBCModule:
                     with open(f'./apps/spacecraft/storage/{filename}', 'r') as file:
                         ats = csv.reader(file)
                         # make commands a list of lists
-                        commands = [list(row) for row in ats]
-                    self.process_ats(commands)
+                        self.ats_commands = [list(row) for row in ats]
+                    self._start_ats_thread()
                 except Exception as e:
                     self.logger.error(f"Error importing ATS: {e}")
                     
-                else:
-                    self.logger.warning(f"Unknown OBC command ID: {command_id}")
+            elif command_id == 16:   # OBC_STOP_ATS
+                self.logger.info("Stopping ATS execution")
+                self._stop_ats()
+                
+            else:
+                self.logger.warning(f"Unknown OBC command ID: {command_id}")
                 
         except struct.error as e:
             self.logger.error(f"Error unpacking OBC command {command_id}: {e}")
+
+    def _start_ats_thread(self):
+        """Start ATS execution in a separate thread"""
+        if self.ats_thread and self.ats_thread.is_alive():
+            self.logger.warning("ATS already running, stopping current execution")
+            self._stop_ats()
+            
+        self.ats_running = True
+        self.ats_thread = threading.Thread(target=self._execute_ats)
+        self.ats_thread.daemon = True
+        self.ats_thread.start()
+        self.logger.info("ATS execution thread started")
+
+    def _stop_ats(self):
+        """Stop ATS execution"""
+        self.ats_running = False
+        if self.ats_thread and self.ats_thread.is_alive():
+            self.ats_thread.join(timeout=1.0)
+        self.ats_commands = []
+        self.logger.info("ATS execution stopped")
+
+    def _execute_ats(self):
+        """Execute ATS commands in a separate thread"""
+        self.logger.info("Starting ATS execution")
+        
+        try:
+            for command in self.ats_commands:
+                if not self.ats_running:
+                    self.logger.info("ATS execution stopped by command")
+                    break
+                    
+                command[0] = int(command[0])
+                command[1] = int(command[1])
+                command_time_delta = command[0] - int((self.current_sim_time - self.epoch).total_seconds())
+                
+                if command_time_delta < 0:
+                    self.logger.warning(f"ATS command {command[1]} at {command[0]} is in the past")
+                    continue
+                    
+                while command_time_delta > 0 and self.ats_running:
+                    time.sleep(SIM_CONFIG['time_step'])
+                    self.logger.debug(f"Time to command_id {command[1]}: {command_time_delta} seconds")
+                    command_time_delta = command[0] - int((self.current_sim_time - self.epoch).total_seconds())
+                
+                if not self.ats_running:
+                    break
+                    
+                # Route command to appropriate subsystem
+                if 10 <= command[1] <= 19:
+                    self.process_ats_command(command[1], command[2])
+                else:
+                    if 20 <= command[1] <= 29:
+                        self.subsystems['power'].process_ats_command(command[1], command[2])
+                    elif 30 <= command[1] <= 39:
+                        self.subsystems['adcs'].process_ats_command(command[1], command[2])
+                    elif 40 <= command[1] <= 49:
+                        self.subsystems['comms'].process_ats_command(command[1], command[2])
+                    elif 50 <= command[1] <= 59:
+                        self.subsystems['payload'].process_ats_command(command[1], command[2])
+                    elif 60 <= command[1] <= 69:
+                        self.subsystems['datastore'].process_ats_command(command[1], command[2])
+                    else:
+                        self.logger.warning(f"Unhandled command ID: {command[1]}")
+                        
+        except Exception as e:
+            self.logger.error(f"Error in ATS execution: {e}")
+        finally:
+            self.ats_running = False
+            self.logger.info("ATS execution completed")
+
+    def _reset(self):
+        """Reset OBC to initial state"""
+        self._stop_ats()  # Stop any running ATS
+        self.alive = False
+        config = SPACECRAFT_CONFIG['spacecraft']['initial_state']['obc']
+        self.state = config['state']
+        self.mode = config['mode']
+        self.heater_setpoint = config['heater_setpoint']
+        self.power_draw = config['power_draw']
+        self.uptime = config['uptime']
+        self.alive = True
+        self.logger.info("OBC reset complete")
+
+    def get_mode(self):
+        """Get the current OBC mode"""
+        return self.mode
+
+    def get_power_draw(self):
+        """Get current power draw in Watts"""
+        return self.power_draw
+    
+    def set_uptime(self, uptime):
+        """Set the uptime in seconds"""
+        self.uptime = uptime
+        
+    def get_uptime(self):
+        """Get the uptime in seconds"""
+        return self.uptime
+
+    def update(self, current_time):
+        """Update OBC state with current simulation time"""
+        self.current_sim_time = current_time
 
     def process_ats_command(self, command_id, command_data):
         """Process OBC commands (Command_ID range 10-19)"""
@@ -131,79 +243,14 @@ class OBCModule:
                 with open(f'./apps/spacecraft/storage/{filename}', 'r') as file:
                     ats = csv.reader(file)
                     # make commands a list of lists
-                    commands = [list(row) for row in ats]
-                self.process_ats(commands)
+                    self.ats_commands = [list(row) for row in ats]
+                self._start_ats_thread()
             except Exception as e:
                 self.logger.error(f"Error importing ATS: {e}")
 
+        elif command_id == 16:   # OBC_STOP_ATS
+            self.logger.info("Stopping ATS execution")
+            self._stop_ats()
+
         else:
             self.logger.warning(f"Unknown OBC command ID: {command_id}")
-                
-    def process_ats(self, commands):
-        """Process ATS commands"""
-        while self.alive:
-            for command in commands:
-                command[0] = int(command[0])
-                command[1] = int(command[1])
-                command_time_delta = command[0] - int((self.current_sim_time - self.epoch).total_seconds())
-                self.logger.debug(f"Current time: {self.current_sim_time}")
-                self.logger.debug(f"Epoch: {self.epoch}")
-                self.logger.debug(f"Current time delta: {int((self.current_sim_time - self.epoch).total_seconds())}")
-                self.logger.debug(f"Command time: {command[0]}")
-                self.logger.debug(f"Command time delta: {command_time_delta}")
-                if command_time_delta < 0:
-                    self.logger.warning(f"ATS command {command[1]} at {command[0]} is in the past")
-                else:
-                    while command_time_delta > 0:
-                        time.sleep(SIM_CONFIG['time_step'])
-                        self.logger.debug(f"Time to command_id {command[1]}: {command_time_delta} seconds")
-                        command_time_delta = command[0] - int((self.current_sim_time - self.epoch).total_seconds())
-                        
-                    if 10 <= command[1] <= 19:
-                        self.process_ats_command(command[1], command[2])
-                    else:
-                        # Route to appropriate subsystem's ats_command processor
-                        if 20 <= command[1] <= 29:
-                            self.subsystems['power'].process_ats_command(command[1], command[2])
-                        elif 30 <= command[1] <= 39:
-                            self.subsystems['adcs'].process_ats_command(command[1], command[2])
-                        elif 40 <= command[1] <= 49:
-                            self.subsystems['comms'].process_ats_command(command[1], command[2])
-                        elif 50 <= command[1] <= 59:
-                            self.subsystems['payload'].process_ats_command(command[1], command[2])
-                        elif 60 <= command[1] <= 69:
-                            self.subsystems['datastore'].process_ats_command(command[1], command[2])
-                        else:
-                            self.logger.warning(f"Unhandled command ID: {command[1]}")
-
-    def _reset(self):
-        """Reset OBC to initial state"""
-        self.alive = False
-        config = SPACECRAFT_CONFIG['spacecraft']['initial_state']['obc']
-        self.state = config['state']
-        self.mode = config['mode']
-        self.heater_setpoint = config['heater_setpoint']
-        self.power_draw = config['power_draw']
-        self.uptime = config['uptime']
-        self.alive = True
-        self.logger.info("OBC reset complete")
-
-    def get_mode(self):
-        """Get the current OBC mode"""
-        return self.mode
-
-    def get_power_draw(self):
-        """Get current power draw in Watts"""
-        return self.power_draw
-    
-    def set_uptime(self, uptime):
-        """Set the uptime in seconds"""
-        self.uptime = uptime
-        
-    def get_uptime(self):
-        """Get the uptime in seconds"""
-        return self.uptime
-
-    def update(self, current_time):
-        """Update OBC state with current simulation time"""
-        self.current_sim_time = current_time
